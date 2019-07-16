1956a55e1a8246afba7e8b602a92fbb72d3e562d
Justin Collum <jcollum@gmail.com>
2019-07-16 12:27:53 -0700

    test for challenge 1 is passing

--- src/Breadcrumb.tsx
+++ src/Breadcrumb.tsx
@@ -4,8 +4,34 @@ export interface Props {
   values: number[];
 }
 
+export function roundAndPad(n:number, digits:number) {
+  var negative = false;
+  if (digits === undefined) {
+      digits = 0;
+  }
+      if( n < 0) {
+      negative = true;
+    n = n * -1;
+  }
+  var multiplicator = Math.pow(10, digits);
+  n = parseFloat((n * multiplicator).toFixed(11));
+  n = (Math.round(n) / multiplicator).toFixed(3);
+  if( negative ) {    
+      n = (n * -1).toFixed(2);
+  }
+  return n;
+}
+
 export class Breadcrumb extends React.PureComponent<Props> {
   render() {
-    return <div />;
+    return (
+      <div className="breadcrumb">
+        <ul>
+          {this.props.values.map((x, i) => {
+            return <li key={i}>{roundAndPad(x, 4)}</li>;
+          })}
+        </ul>
+      </div>
+    );
   }
 }
